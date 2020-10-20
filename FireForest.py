6e1a19da363047cb6ca111ab588cb60edf98323b
Alon <alonm774@gmail.com>
2020-10-20 18:55:57 +0300

    added documentation

--- FireForest.py
+++ FireForest.py
@@ -5,23 +5,36 @@ class FireTree:
     burn_time = 0
 
     def __init__(self, row, col):
+        """
+        :param row: row position of the tree in the forest matrix
+        :param col: col position of the tree in the forest matrix
+        """
         self.row = row
         self.col = col
-        self.time = FireTree.burn_time
+        self.time_to_burn = FireTree.burn_time
 
     def decrease_time(self):
-        self.time = self.time-1
+        self.time_to_burn = self.time_to_burn-1
 
 
 class FireForest:
 
-    def __init__(self, forest_x, forest_y, burn_time, spreading_chance):
+    def __init__(self, forest_x, forest_y, burn_time, spreading_probability):
+        """
+        forest_mat: boolean matrix representing the forest by False meaning
+                      the tree isn't burned
+        on_fire: list of trees that are currently on fire
+        :param forest_x: number of rows in the matrix
+        :param forest_y: number of columns in the matrix
+        :param burn_time: the time it takes a tree to burn
+        :param spreading_probability: probability of fire to spread between trees
+        """
         FireTree.burn_time = burn_time
         self.forest_x = forest_x
         self.forest_y = forest_y
-        self.spreading_chance = spreading_chance
-        self.forest_arr = [[False for _ in range(self.forest_y)] for _ in range(self.forest_x)]
-        self.on_fire_arr = []
+        self.spreading_probability = spreading_probability
+        self.forest_mat = [[False for _ in range(self.forest_y)] for _ in range(self.forest_x)]
+        self.on_fire = []
         self.time = 0
         self.number_burned_trees = 1
 
@@ -31,41 +44,48 @@ class FireForest:
         return [(x, y) for (x, y) in neighbors if 0 <= x < self.forest_x and 0 <= y < self.forest_y]
 
     def __step(self):
-        for i in range(len(self.on_fire_arr)):
-            curr_tree = self.on_fire_arr.pop(0)
+        """
+        for each burning tree, neighboring trees might get burned according to the spreading probability
+        """
+        for i in range(len(self.on_fire)):
+            curr_tree = self.on_fire.pop(0)
             for (neighbor_row, neighbor_col) in self.__get_legal_neighbors(curr_tree.row, curr_tree.col):
-                if random.randint(1, 100) <= self.spreading_chance and not self.forest_arr[neighbor_row][neighbor_col]:
-                    self.forest_arr[neighbor_row][neighbor_col] = True
-                    self.on_fire_arr.append(FireTree(neighbor_row, neighbor_col))
+                if random.randint(1, 100) <= self.spreading_probability and\
+                        not self.forest_mat[neighbor_row][neighbor_col]:
+                    self.forest_mat[neighbor_row][neighbor_col] = True
+                    self.on_fire.append(FireTree(neighbor_row, neighbor_col))
                     self.number_burned_trees += 1
 
             curr_tree.decrease_time()
             if curr_tree.time > 0:
-                self.on_fire_arr.append(curr_tree)
+                self.on_fire.append(curr_tree)
 
     def conclusion(self):
         total_trees = self.forest_x * self.forest_y
         alive_trees = total_trees - self.number_burned_trees
         survival_rate = alive_trees/total_trees
+        print("\nResults:")
         print("Total Time : " + str(self.time))
         print("Total Trees: " + str(total_trees))
         print("Burned Trees: " + str(self.number_burned_trees))
         print("Non Burned Trees: " + str(alive_trees))
-        print("Survival Rate: " + str(survival_rate))
+        print("Survival Rate: " + str(survival_rate) + "\n")
 
     def simulation(self):
+        """
+        picks a tree randomly that starts to burn, and run each step until the forest stops to burn
+        """
         start_tree_x = random.randint(0, self.forest_x-1)
         start_tree_y = random.randint(0, self.forest_y-1)
-        self.forest_arr[start_tree_x][start_tree_y] = True
-        self.on_fire_arr = [FireTree(start_tree_x, start_tree_y)]
-        while len(self.on_fire_arr) > 0:
+        self.forest_mat[start_tree_x][start_tree_y] = True
+        self.on_fire = [FireTree(start_tree_x, start_tree_y)]
+        while len(self.on_fire) > 0:
             self.__step()
             self.time += 1
 
         self.conclusion()
-
-
-
-
-
-
+        import matplotlib.pyplot as plt
+        plt.title('forest')
+        plt.imshow(self.forest_mat)
+        plt.colorbar()
+        plt.show()
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from FireForest import FireForest
 
-if __name__ == "__main__":
-    forest_x, forest_y = 10, 10
-    burn_time = 3
-    spreading_chance = 10
-    fireForest = FireForest(forest_x, forest_y, burn_time, spreading_chance)
-    fireForest.simulation()
 
+def main():
+    """
+    gets user input and start the simulation
+    """
+    while True:
+        forest_x, forest_y = [int(dimension) for dimension in input("Enter Forest Dimensions: ").split()]
+        burn_time = int(input("Enter Tree Burn Time: "))
+        spreading_probability = int(input("Enter Probability of Fire to Spread: "))
+        # forest_x, forest_y = 40, 40
+        # burn_time = 3
+        # spreading_chance = 10
+        FireForest(forest_x, forest_y, burn_time, spreading_probability).simulation()
+
+        if input("Would you like to try again? (y/n): ").lower() != "y":
+            break
+
+
+if __name__ == "__main__":
+    main()
